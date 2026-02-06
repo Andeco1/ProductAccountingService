83a8f07540df6bd554087fe9bb4496fe734f26a1
Yan <Andeco12@yandex.ru>
2026-02-06 14:24:53 +0300

    init project

---
+++ src/main/java/ru/supplyservice/productAccounting/FruitAccountingApplication.java
@@ -0,0 +1,13 @@
+package ru.supplyservice.productAccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/supplyservice/productAccounting/FruitAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.supplyservice.productAccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
