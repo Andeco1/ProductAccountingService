5b1d0e0928a08d73ec531b8894e37307abcd0c39
Yan <Andeco12@yandex.ru>
2026-02-06 14:28:12 +0300

    exception implemented

---
+++ src/main/java/ru/supplyservice/productAccounting/exception/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.supplyservice.productAccounting.exception;
+
+public class EntityNotFoundException extends FruitAccountingException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/supplyservice/productAccounting/exception/FruitAccountingException.java
@@ -0,0 +1,7 @@
+package ru.supplyservice.productAccounting.exception;
+
+public class FruitAccountingException extends RuntimeException {
+    public FruitAccountingException(String message) {
+        super(message);
+    }
+}
